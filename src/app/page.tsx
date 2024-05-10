9e311a26fa703284d9e4234dfc554930d369a5a5
williamstyronejr <itenory@gmail.com>
2024-05-09 20:26:48 -0400

    Created layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,209 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+const BlockBuilder = () => {
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className="flex flex-col flex-nowrap grow">
+      <div className="grow">
+        <button type="button" className="" onClick={() => {}}>
+          New Table
+        </button>
+      </div>
+
+      <div className="shrink-0 w-full h-12">
+        <label
+          className="flex flex-row flex-nowrap w-full h-full items-center border-t border-slate-300 bg-white p-2"
+          htmlFor="search"
+        >
+          <div className="w-6 h-6 mr-2">
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <g>
+                  <circle
+                    cx="10.5"
+                    cy="10.5"
+                    r="6.5"
+                    stroke="#000000"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M19.6464 20.3536C19.8417 20.5488 20.1583 20.5488 20.3536 20.3536C20.5488 20.1583 20.5488 19.8417 20.3536 19.6464L19.6464 20.3536ZM20.3536 19.6464L15.3536 14.6464L14.6464 15.3536L19.6464 20.3536L20.3536 19.6464Z"
+                    fill="#000000"
+                  />
+                </g>
+              </g>
+            </svg>
+          </div>
+
+          <input
+            className="grow"
+            name="search"
+            placeholder="Search for a table name ..."
+            value={search}
+            onChange={(evt) => setSearch(evt.target.value)}
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+const CodeEditor = () => {
+  const [code, setCode] = useState("");
+
+  return (
+    <div className="flex flex-col flex-nowrap grow ">
+      <div className="grow">Code Editor</div>
+
+      <div className="h-12 shrink-0 w-full border-t border-slate-300 bg-white p-2">
+        Status
+      </div>
+    </div>
+  );
+};
+
+const Aside = () => {
+  const [menu, setMenu] = useState("block");
+
+  return (
+    <aside className="flex flex-col flex-nowrap w-80 shrink-0 border-r border-slate-200">
+      <div className="flex flex-row flex-nowrap">
+        <button
+          className="font-semibold w-1/2 border-b-2 text-neutral-500 border-neutral-500/20 disabled:text-black disabled:border-black py-4"
+          disabled={menu === "code"}
+          type="button"
+          onClick={() => setMenu("code")}
+        >
+          Code Editor
+        </button>
+
+        <button
+          className="font-semibold w-1/2 border-b-2 text-neutral-500 border-neutral-500/20 disabled:text-black disabled:border-black py-4"
+          disabled={menu === "block"}
+          type="button"
+          onClick={() => setMenu("block")}
+        >
+          Block Builder
+        </button>
+      </div>
+
+      {menu === "block" ? <BlockBuilder /> : null}
+
+      {menu === "code" ? <CodeEditor /> : null}
+    </aside>
+  );
+};
+
+const Diagram = ({ items }: { items: any[] }) => {
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  const drawTable = (ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "#000000";
+    ctx.roundRect(20, 20, 100, 100, 4);
+    ctx.fill();
+  };
+
+  useEffect(() => {
+    const ctx = ref.current?.getContext("2d");
+
+    if (ctx) {
+      drawTable(ctx);
+    }
+  }, [items]);
+
+  return (
+    <div className="flex flex-col flex-nowrap h-full w-full">
+      <div className="h-12 bg-white shrink-0"> </div>
+
+      <div className="grow">
+        <canvas className="w-full h-full" ref={ref} />
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
+  const [zoom, setZoom] = useState(100);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className="flex flex-row flex-nowrap h-full">
+      <Aside />
+
+      <div className="grow relative bg-[#f9f9f9]">
+        <Diagram
+          items={[
+            { title: "Table Title", fields: [{ name: "id", type: "int" }] },
+          ]}
+        />
+        <div className="absolute bottom-2 w-full h-8">
+          <div className="flex flex-row flex-nowrap items-center">
+            <div className="w-12 h-8 rounded-md bg-white text-slate-600 text-center border border-slate-200 pt-1">
+              {zoom}%
+            </div>
+
+            <button
+              className="w-8 h-8 rounded-md bg-white mx-1 border border-slate-200"
+              type="button"
+              disabled={zoom === 200}
+              onClick={() => {
+                setZoom((old) => old + 25);
+              }}
+            >
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g>
+                  <path
+                    d="M12 6V18"
+                    stroke="#475569"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M6 12H18"
+                    stroke="#475569"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </g>
+              </svg>
+            </button>
+
+            <button
+              className="w-8 h-8 rounded-md bg-white border border-slate-200 disabled:bg-slate"
+              type="button"
+              disabled={zoom === 0}
+              onClick={() => {
+                setZoom((old) => old - 25);
+              }}
+            >
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g>
+                  <path
+                    d="M6 12H18"
+                    stroke="#475569"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </g>
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </section>
   );
 }
